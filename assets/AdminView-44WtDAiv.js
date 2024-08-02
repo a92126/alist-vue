977f07d2caaecfa46171157ec4721add4ce8ff08
root <182859762@qq.com>
2024-08-02 23:40:43 +0800

    播放器设置为剧中

---
+++ assets/AdminView-44WtDAiv.js
@@ -0,0 +1,1 @@
+import{d as a,s as o,u as t,a as n,c,b as r,e as i,f as d,o as e,A as _,L as l}from"./index-5FCBRnv6.js";const m={key:0},u=d("div",{class:"disk-view"},null,-1),h=a({__name:"AdminView",setup(f){const{isReady:s}=o(t());return(p,k)=>n(s)?(e(),c("main",m,[r(_),u])):(e(),i(l,{key:1}))}});export{h as default};
