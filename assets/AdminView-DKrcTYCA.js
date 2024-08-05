6a73563c58020be4fc2fb7c73cf3a403717fb945
anod <anod221@outlook.com>
2024-08-06 00:15:51 +0800

    去掉不需要的文件。之前复制了太多

--- assets/AdminView-DKrcTYCA.js
+++
@@ -1,1 +0,0 @@
-import{d as a,s as o,u as t,a as n,c,b as r,e as i,f as d,o as e,A as _,L as l}from"./index-pnlBj1kV.js";const m={key:0},u=d("div",{class:"disk-view"},null,-1),h=a({__name:"AdminView",setup(f){const{isReady:s}=o(t());return(p,k)=>n(s)?(e(),c("main",m,[r(_),u])):(e(),i(l,{key:1}))}});export{h as default};
